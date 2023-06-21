67655f0ef10b63923092aa437561b0c121d7fcb9
sukjune.choi <sukjune.choi@gmail.com>
2023-06-21 14:20:19 +0900

    add

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
-console.log("server start")
+console.log("server start!")
